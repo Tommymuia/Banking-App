c44eb8f39cd4e781d985307b0a95402e5b3d6385
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-14 17:50:01 +0300

    added to mailer.py

--- app/mailer.py
+++ app/mailer.py
@@ -20,16 +20,16 @@ def send_email(to_email, subject, body):
         recipients=[to_email],
         body=body,
         sender=current_app.config["MAIL_DEFAULT_SENDER"]
-
     )
-    mail.send(msg)    
+    mail.send(msg)
 
-    def send_transaction_alert(to_email, amount, transaction_type):
-        """
-        Sends a banking transaction alert email
-        """
-        subject = "Transaction Alert"
-        body = (
+
+def send_transaction_alert(to_email, amount, transaction_type):
+    """
+    Sends a banking transaction alert email
+    """
+    subject = "Transaction Alert"
+    body = (
         f"Dear Customer,\n\n"
         f"A {transaction_type} of ${amount} has occurred on your account.\n\n"
         f"If this was not you, please contact support immediately.\n\n"
